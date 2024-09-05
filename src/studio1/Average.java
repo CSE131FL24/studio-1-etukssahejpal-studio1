9c5e066e9939b2d3abf3ebb4c536b317c0e8e73b
aashnasahejpal <aashnasahejpal@localhost>
2024-09-05 12:35:36 -0500

    We completed Studio1!

---
+++ src/studio1/Average.java
@@ -0,0 +1,21 @@
+package studio1;
+
+import java.util.Scanner;
+
+public class Average {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter an integer");
+		int n1 = in.nextInt();
+		System.out.println("Enter a second integer");
+		int n2 = in.nextInt();
+		double avg =  (n1 + n2) / 2; 
+		System.out.println("The average is " + avg);
+		
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
